12281d04279f7fea5d18dcd0f7720b4c243fb86b
xuleiyu <yuxulei@bytedance.com>
2019-12-29 11:15:12 +0800

    添加删除video接口

---
+++ realMatch-OC/realMatch-OC/App/Home/Profile/RMDeleteVideoAPI.swift
@@ -0,0 +1,13 @@
+//
+//  RMDeleteVideoAPI.swift
+//  realMatch-OC
+//
+//  Created by xulei on 2019/12/4.
+//  Copyright © 2019 qingting. All rights reserved.
+//
+
+import UIKit
+
+class RMDeleteVideoAPI: NSObject {
+
+}
